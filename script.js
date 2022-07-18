511597d899d8c7a679635a1981e31b68f8d9395f
Przemyslaw Dlugoszewski-Tamon <przemyslaw.tamon@cloudity.pl>
2022-07-18 16:52:05 +0200

    present the overall plan of the project

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+
+    giveIntro() - present to the player what's the game about
+    Repeat 5 times the following:
+        presentQuestion() - Ask for the value (rock, paper, scissors)
+        getUserInput() - Get input from the user
+        parseUserInput() - Parse the input (process further / report error)
+        generateComputerValue() - Generate computer value
+        compareUserWithComputerValues() - Compare it with generated value
+        saveSingleGameResult() - Score point to either player
+    presentGameResult() - present results to the player
+
+*/
