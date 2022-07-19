fa971ff7e0d76ce0e105170b941fbbe9c45be09a
Przemyslaw Dlugoszewski-Tamon <przemyslaw.tamon@cloudity.pl>
2022-07-19 09:49:04 +0200

    store user and computer choices

--- script.js
+++ script.js
@@ -12,9 +12,21 @@
 
 */
 
+const ROUNDS_NUMBER = 5;
+
+const CHOICE_ROCK = 'ROCK';
+const CHOICE_PAPER = 'PAPER';
+const CHOICE_SCISSORS = 'SCISSORS';
+const VALID_CHOICES = [CHOICE_ROCK, CHOICE_PAPER, CHOICE_SCISSORS];
+const WHAT_BEATS_WHAT = {
+    CHOICE_ROCK: CHOICE_SCISSORS,
+    CHOICE_PAPER: CHOICE_ROCK,
+    CHOICE_SCISSORS: CHOICE_PAPER
+};
+
 giveIntro();
-for (let i = 0 ; i < 5 ; i++) {
-    presentQuestion();
+for (let i = 0 ; i < ROUNDS_NUMBER ; i++) {
+    playSingeGame();
 }
 
 function giveIntro() {
@@ -23,21 +35,31 @@ Welcome to the Rock, Paper and Scissors.
 It's a simple game where you pick one of the options and play against the computer.
 
 Rules:
-- 5 rounds
+- ${ROUNDS_NUMBER} rounds
 - each won run gives you 1 point
 - the player with higher amount of points wins
     `);
 }
 
-function presentQuestion() {
-    let choice = prompt('Make your choice (rock, paper, scissors): ');
+function playSingeGame() {
+    let playerChoice = promptForChoice().toString().toUpperCase();
+    console.log('playerChoice: ' + playerChoice);
+    if (isChoiceValid(playerChoice)) {
+        let computerChoice = shuffleComputerChoice();
+        console.log('computerChoice: ' + computerChoice);
+    }
 }
 
-const choiceRock = 'ROCK';
-const choicePaper = 'PAPER';
-const choiceScissors = 'SCISSORS';
+function promptForChoice() {
+    return prompt('Make your choice (rock, paper, scissors): ');
+}
 
-function isInputValid(input) {
-    inputUpper = input.toString().toUpper();
-    return [choiceRock, choicePaper, choiceScissors].includes(inputUpper);
+function isChoiceValid(input) {
+    inputUpper = input;
+    return VALID_CHOICES.includes(inputUpper);
+}
+
+function shuffleComputerChoice() {
+    let choiceIndex = parseInt(Math.floor(Math.random() * 3));
+    return VALID_CHOICES[choiceIndex];
 }
